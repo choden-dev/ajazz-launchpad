744f360e9ecc781589205d7306e3476a82fb47a8
Benson Cho <100653148+choden-dev@users.noreply.github.com>
2025-11-23 10:47:35 +1300

    fix: remove profanity

--- config-frontend/src/views/initialise.rs
+++ config-frontend/src/views/initialise.rs
@@ -3,7 +3,7 @@ use crate::messages::Messages;
 pub struct Initialise;
 impl Initialise {
     pub fn view(&'_ self) -> iced::Element<'_, Messages> {
-        iced::widget::column![iced::widget::text("FUCK YOU")]
+        iced::widget::column![iced::widget::text("Loading...")]
             .padding(10)
             .spacing(10)
             .into()
